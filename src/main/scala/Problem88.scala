309bd5b05323d7dd9a8dc97244d19742800c7d5a
Andrey-L-E-V <andrey.levchenko.1996@mail.ru>
2024-03-12 20:27:45 +0300

    first commit

---
+++ src/main/scala/Problem88.scala
@@ -0,0 +1,21 @@
+object Problem88 {
+  def merge(nums1: Array[Int], m: Int, nums2: Array[Int], n: Int): Unit = {
+    def f(nums11: Array[Int], nums22: Array[Int]): Array[Int] = {
+      if (nums11.nonEmpty && nums22.nonEmpty)
+        if (nums11.head < nums22.head)
+          nums11.head +: f(nums11.tail, nums22)
+        else
+          nums22.head +: f(nums11, nums22.tail)
+
+      else if (nums11.nonEmpty) nums11
+      else if (nums22.nonEmpty) nums22
+      else Array[Int]()
+    }
+
+    val nums11 = new Array[Int](m)
+    for (i <- 0 until m) nums11(i) = nums1(i)
+
+    val nums = f(nums11, nums2)
+    for (i <- nums1.indices) nums1(i) = nums(i)
+  }
+}
